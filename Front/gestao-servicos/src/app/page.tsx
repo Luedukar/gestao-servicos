82c851e863b41298dd608b6db33fd853ef987c09
Luiz <luedukar@gmail.com>
2026-07-28 17:53:44 -0300

    renderização do componente costumerDashboard

--- Front/gestao-servicos/src/app/page.tsx
+++ Front/gestao-servicos/src/app/page.tsx
@@ -1,4 +1,15 @@
+import { CustomerDashboard } from '@/components/ServerComponents/CustomerDashboard';
 
 export default function HomePage() {
-  return <div></div>;
+  return (
+    <main className='w-full'>
+      <h1 className='text-2xl font-semibold text-slate-800 mb-4'>
+        Gestão de Serviços
+      </h1>
+      <p className='text-slate-600 mb-6'>
+        Visualização de clientes cadastrados.
+      </p>
+      <CustomerDashboard />
+    </main>
+  );
 }
